cb22a049e38ed320cb0960120ceeaa9d1bff4795
Rishona9 <96752903+Rishona9@users.noreply.github.com>
2022-10-13 11:02:33 -0400

    displayModal function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 const gallery = document.getElementById("gallery");
+const card = document.querySelectorAll(".card");
 let employees = [];
 
 //Checks status of request
@@ -37,4 +38,42 @@ function generateEmployees() {
   gallery.insertAdjacentHTML("beforeend", employeeHTML);
 }
 
-//
+//Displays employee information in a modal when card is clicked
+function displayModal() {
+  let modalContainer = "";
+  modalContainer.forEach((data, index) => {
+    modalContainer += `
+    <div class="modal-container data-index="${index}">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                    <img class="modal-img" src="${data.picture.medium}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+                    <p class="modal-text">${data.email}</p>
+                    <p class="modal-text cap">${data.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${data.phone}</p>
+                    <p class="modal-text">${data.location.street.number} ${data.location.street.name}, ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${data.dob.date}</p>
+            </div>
+        </div>
+    </div>
+    `;
+  });
+  gallery.insertAdjacentHTML("beforeend", modalContainer);
+
+  let modal = document.querySelectorAll(".modal");
+  let closeBtn = document.getElementById("modal-close-btn");
+
+  card.onclick = function () {
+    modal.style.display = "block";
+  };
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+  window.onclick = function (e) {
+    if (e.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+}
